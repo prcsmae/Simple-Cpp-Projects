65981eea836c8a8edf28d501a19ef62743daaa40
prcsmae <preciousmaejomuad1@gmail.com>
2024-01-15 13:29:10 +0800

    updated the code

---
+++ basic_calculator.cpp
@@ -0,0 +1,12 @@
+/*Rock paper scissors game using C++*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Enter a number:";
+  int num;
+  cin >> num;
+  cout << "The number you entered is " << num << ".";
+  return 0;
+}
