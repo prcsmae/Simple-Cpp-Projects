ae61e48a603d5b90a76e579f777863178bf97cb2
prcsmae <preciousmaejomuad1@gmail.com>
2024-01-16 11:03:02 +0800

    added the coinToss function to improve readability

--- rockpaperscissors.cpp
+++ rockpaperscissors.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 char toLower(char c);
 void Clear() { cout << "\x1B[2J\x1B[H"; }
+int coinToss (string choice1);
 
 int main() {
     Clear();
@@ -17,55 +18,9 @@ int main() {
     cout << "Choose a side of the coin to determine which player plays first.\n\n";
     
     string choice1, choice2, gameChoice1, gameChoice2;
-    cout << "Player 1 choice (Heads or Tails): ";
+     cout << "Player 1 choice (Heads or Tails): ";
     cin >> choice1;
-    // Transforms the input string in the case the user inputs uppercase
-    transform(choice1.begin(), choice1.end(), choice1.begin(), toLower); 
-
-    if (choice1 == "heads"){
-        cout << "\nPlayer 2's choice will now be automatically tails.\n";
-        choice2 = "tails";
-    } else if (choice1 == "tails"){
-        cout << "\nPlayer 2's choice will now be automatically heads.\n";
-        choice2 = "heads";
-    }
-
-    // Creates an object rd of the class random_device
-    random_device rd;
-
-    // Create a random number engine using the random_device
-    default_random_engine engine(rd());
-
-    // Create a distribution for the range of random numbers you want
-    uniform_int_distribution<int> distribution(1, 2);
-
-    // Generate a random number
-    int coinRand = distribution(engine);
-
-    int turn, score1 = 0, score2 = 0;
-
-    switch (coinRand){
-        case 1:
-            cout << "\nHeads wins.";
-            if (choice1 == "heads"){
-                cout << "\nPlayer 1 will go first.";
-                turn = 1;
-            } else if (choice2 == "heads"){
-                cout << "\nPlayer 2 will go first.";
-                turn = 2;
-            }
-            break;
-        case 2:
-            cout << "\nTails wins.";
-            if (choice1 == "tails"){
-                cout << "\nPlayer 1 will go first.";
-                turn = 1;
-            } else if (choice2 == "tails"){
-                cout << "\nPlayer 2 will go first.";
-                turn = 2;
-            }
-            break;
-    }
+    int turn = coinToss(choice1), score1 = 0, score2 = 0;
 
     cout << "\n\n---------------------------------------------";
     cout << "\n\tGame will now start.\n";
@@ -124,4 +79,56 @@ char toLower(char c) {
     /* unsigned char ensures that the value of c is treated as an unsigned character
     (to handle the possibility of negative char values correctly) before being passed to tolower.*/
     return tolower(static_cast<unsigned char>(c));
+}
+
+int coinToss (string choice1){
+    string choice2;
+    // Transforms the input string in the case the user inputs uppercase
+    transform(choice1.begin(), choice1.end(), choice1.begin(), toLower); 
+
+    if (choice1 == "heads"){
+        cout << "\nPlayer 2's choice will now be automatically tails.\n";
+        choice2 = "tails";
+    } else if (choice1 == "tails"){
+        cout << "\nPlayer 2's choice will now be automatically heads.\n";
+        choice2 = "heads";
+    }
+
+    // Creates an object rd of the class random_device
+    random_device rd;
+
+    // Create a random number engine using the random_device
+    default_random_engine engine(rd());
+
+    // Create a distribution for the range of random numbers you want
+    uniform_int_distribution<int> distribution(1, 2);
+
+    // Generate a random number
+    int coinRand = distribution(engine);
+
+    int turn;
+
+    switch (coinRand){
+        case 1:
+            cout << "\nHeads wins.";
+            if (choice1 == "heads"){
+                cout << "\nPlayer 1 will go first.";
+                turn = 1;
+            } else if (choice2 == "heads"){
+                cout << "\nPlayer 2 will go first.";
+                turn = 2;
+            }
+            break;
+        case 2:
+            cout << "\nTails wins.";
+            if (choice1 == "tails"){
+                cout << "\nPlayer 1 will go first.";
+                turn = 1;
+            } else if (choice2 == "tails"){
+                cout << "\nPlayer 2 will go first.";
+                turn = 2;
+            }
+            break;
+    }
+    return turn;
 }
