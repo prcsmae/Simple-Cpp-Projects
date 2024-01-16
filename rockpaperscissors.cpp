049c616d6e6341793c27858b2aa1c9b08e2d0147
prcsmae <preciousmaejomuad1@gmail.com>
2024-01-16 10:58:01 +0800

    added the actual rock paper scissors part

--- rockpaperscissors.cpp
+++ rockpaperscissors.cpp
@@ -1,27 +1,34 @@
 /*Program to play rock paper scissors between two players*/
 
 #include <iostream>
-#include <random>
-#include <cctype>
-#include <algorithm>
+#include <stdlib.h>
+#include <random> // for the random number generator
+#include <cctype> // for tolower
+#include <algorithm> // for transform
 
 using namespace std;
 
 char toLower(char c);
+void Clear() { cout << "\x1B[2J\x1B[H"; }
 
 int main() {
+    Clear();
     cout << "This program lets you play rock paper scissors between two players.\n";
     cout << "Choose a side of the coin to determine which player plays first.\n\n";
-
+    
     string choice1, choice2, gameChoice1, gameChoice2;
     cout << "Player 1 choice (Heads or Tails): ";
     cin >> choice1;
     // Transforms the input string in the case the user inputs uppercase
     transform(choice1.begin(), choice1.end(), choice1.begin(), toLower); 
 
-    cout << "Player 2 choice (Heads or Tails): ";
-    cin >> choice2;
-    transform(choice2.begin(), choice2.end(), choice2.begin(), toLower); 
+    if (choice1 == "heads"){
+        cout << "\nPlayer 2's choice will now be automatically tails.\n";
+        choice2 = "tails";
+    } else if (choice1 == "tails"){
+        cout << "\nPlayer 2's choice will now be automatically heads.\n";
+        choice2 = "heads";
+    }
 
     // Creates an object rd of the class random_device
     random_device rd;
@@ -35,7 +42,7 @@ int main() {
     // Generate a random number
     int coinRand = distribution(engine);
 
-    int turn;
+    int turn, score1 = 0, score2 = 0;
 
     switch (coinRand){
         case 1:
@@ -60,32 +67,56 @@ int main() {
             break;
     }
 
-    switch (turn){
-        case 1:
-            cout << "\nPlayer 1 choice (rock, paper, or scissors): ";
-            cin >> gameChoice1;
-            transform(gameChoice1.begin(), gameChoice1.end(), gameChoice1.begin(), toLower); 
+    cout << "\n\n---------------------------------------------";
+    cout << "\n\tGame will now start.\n";
+    cout << "---------------------------------------------\n";
 
-            cout << "\nPlayer 2 choice (rock, paper, or scissors): ";
-            cin >> gameChoice2;
-            transform(gameChoice2.begin(), gameChoice2.end(), gameChoice2.begin(), toLower);
+    for (int rounds = 0; rounds <3 ; rounds ++){
+        switch (turn){
+            case 1:
+                cout << "\nPlayer 1 choice (rock, paper, or scissors): ";
+                cin >> gameChoice1;
+                transform(gameChoice1.begin(), gameChoice1.end(), gameChoice1.begin(), toLower); 
 
-            break;
-        case 2:
-            cout << "\nPlayer 2 choice (rock, paper, or scissors): ";
-            cin >> gameChoice2;
-            transform(gameChoice2.begin(), gameChoice2.end(), gameChoice2.begin(), toLower);
+                cout << "\nPlayer 2 choice (rock, paper, or scissors): ";
+                cin >> gameChoice2;
+                transform(gameChoice2.begin(), gameChoice2.end(), gameChoice2.begin(), toLower);
 
-            cout << "\nPlayer 1 choice (rock, paper, or scissors): ";
-            cin >> gameChoice1;
-            transform(gameChoice1.begin(), gameChoice1.end(), gameChoice1.begin(), toLower); 
+                break;
+            case 2:
+                cout << "\nPlayer 2 choice (rock, paper, or scissors): ";
+                cin >> gameChoice2;
+                transform(gameChoice2.begin(), gameChoice2.end(), gameChoice2.begin(), toLower);
 
-            break;
+                cout << "\nPlayer 1 choice (rock, paper, or scissors): ";
+                cin >> gameChoice1;
+                transform(gameChoice1.begin(), gameChoice1.end(), gameChoice1.begin(), toLower); 
+
+                break;
+        }
+
+        if (gameChoice1 == gameChoice2){
+            cout << "It's a tie!\n";
+            rounds--;
+        } else if (gameChoice1 == "rock" && gameChoice2 == "scissors" || gameChoice1 == "paper" && gameChoice2 == "rock" ||
+                    gameChoice1 == "scissors" && gameChoice2 == "paper"){
+                        score1++;
+                        cout << "\nPlayer 1 wins!\n";
+                        cout << "\nCurrent score: \nPlayer 1: " << score1 << "\nPlayer 2: " << score2;
+                        turn = 2;
+                    }
+        else{
+            cout << "\nPlayer 2 wins!\n";
+            score2++;
+            cout << "\nCurrent score: \nPlayer 1: " << score1 << "\nPlayer 2: " << score2;
+            turn = 1;
+        }
     }
-
-
-
-
+    if (score1 > score2){
+        cout << "\n\nPlayer 1 is the ultimate winner! Try again next time player 2.";
+    } else if (score2 > score1){
+        cout << "\n\nPlayer 2 is the ultimate winner! Try again next time player 1.";
+    }
 }
 
 char toLower(char c) {
